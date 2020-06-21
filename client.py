c975dd47decfe7b4b8eb03b82cb59c7c24398c48
lilthanasis <53539630+olilthanasis@users.noreply.github.com>
2020-06-21 22:26:33 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 
 
-IP = socket.gethostname() #Test IP
+IP = socket.gethostname()#Test IP
 PORT = 5000
 
 
@@ -13,10 +13,12 @@ s.send(bytes(file_name,"utf-8"))
 file_exists =s.recv(1024).decode("utf-8")
 if file_exists == "True":
     print(f"{file_name} found. We are sending it over")
-file_data = s.recv(2**32)
-with open(file_name,"wb") as file:
-    file.write(file_data)
-s.close()
-
-
+    file_data = s.recv(2**32)
+    with open('test.mp3',"wb") as file:
+        file.write(file_data)
+    print(f"file saved at {file_name}")
+    s.close()
+else:
+    print("file was not found")
+    s.close()
 
