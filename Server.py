b9b835c8d88c08e3574889f94788065b9a2b401b
lilthanasis <53539630+olilthanasis@users.noreply.github.com>
2020-06-21 21:49:50 +0300

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,22 @@
+import socket
+import os
+import time
+
+
+IP = socket.gethostname()
+PORT = 5000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((IP, PORT))
+s.listen(5)
+while True:
+    connected_socket, addr = s.accept()
+    connected_socket.send(bytes("Connected successfully; please enter the name of the desired file","utf-8"))
+
+    file_name = connected_socket.recv(1024).decode("utf-8")
+    if os.path.exists(file_name):
+        connected_socket.send(bytes("True", "utf-8"))
+        with open(file_name,"rb") as file:
+            file_bytes = file.read()
+            connected_socket.send(file_bytes)
+
---
+++ client.py
@@ -0,0 +1,22 @@
+import socket
+
+
+IP = socket.gethostname()
+PORT = 5000
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((IP, PORT))
+print(s.recv(1024).decode("utf-8"))
+file_name = input(">>>")
+s.send(bytes(file_name,"utf-8"))
+file_exists =s.recv(1024).decode("utf-8")
+if file_exists == "True":
+    print(f"{file_name} found. We are sending it over")
+file_data = s.recv(2**32)
+with open(file_name,"wb") as file:
+    file.write(file_data)
+s.close()
+
+
+
