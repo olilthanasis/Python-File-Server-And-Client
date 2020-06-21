e399f36d80803e3251d472bcdd58b48ec255022e
lilthanasis <53539630+olilthanasis@users.noreply.github.com>
2020-06-21 22:27:19 +0300

    Update Server.py

--- Server.py
+++ Server.py
@@ -3,7 +3,7 @@ import os
 import time
 
 
-IP = socket.gethostname() #Test IP
+IP = socket.gethostname()
 PORT = 5000
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,11 +12,11 @@ s.listen(5)
 while True:
     connected_socket, addr = s.accept()
     connected_socket.send(bytes("Connected successfully; please enter the name of the desired file","utf-8"))
-
     file_name = connected_socket.recv(1024).decode("utf-8")
     if os.path.exists(file_name):
         connected_socket.send(bytes("True", "utf-8"))
         with open(file_name,"rb") as file:
             file_bytes = file.read()
             connected_socket.send(file_bytes)
-
+    else:
+        connected_socket.send(bytes("False", "utf-8"))
