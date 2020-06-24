1483d3957808b8cd9d75790b027bb9dbe0c189f3
lilthanasis <53539630+olilthanasis@users.noreply.github.com>
2020-06-24 18:43:48 +0300

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,46 +1,42 @@
 import socket
 import os
 import pickle
+import threading
 
 
 IP = socket.gethostname()
 PORT = 5000
-
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((IP, PORT))
 s.listen(5)
 
 
-def server():
-    while True:
-        connected_socket, addr = s.accept()
-        connected_socket.send(bytes("Connected successfully; please enter the name of the desired file","utf-8"))
-        try:
-            if connected_socket.recv(1024).decode("utf-8") == "DOWNLOAD":
-                list_of_files = os.listdir(r"C:\Users\mitth\PycharmProjects\sockets")
-                data = pickle.dumps(list_of_files)
-                connected_socket.send(data)
-                file_name = connected_socket.recv(1024).decode("utf-8")
-                if os.path.exists(file_name):
-                    connected_socket.send(bytes("True", "utf-8"))
-                    with open(file_name,"rb") as file:
-                        file_bytes = file.read()
-                        connected_socket.send(file_bytes)
-                else:
-                    connected_socket.send(bytes("False", "utf-8"))
-            else:
-                list_of_files = os.listdir(r"C:\Users\mitth\PycharmProjects\sockets")
-                data = pickle.dumps(list_of_files)
-                connected_socket.send(data)
-                file_head = connected_socket.recv(2**32).decode("utf-8")
-                file_data = connected_socket.recv(2**32)
-                with open(file_head, "wb") as f:
-                    f.write(bytes(file_data))
-                    f.close()
-        except ConnectionResetError:
-            print("handling connection error")
-            server()
+def server(client):
+    client.send(bytes("Connected successfully; please enter the name of the desired file","utf-8"))
+    if client.recv(1024).decode("utf-8") == "DOWNLOAD":
+        list_of_files = os.listdir(r"C:\Users\mitth\PycharmProjects\sockets")
+        data = pickle.dumps(list_of_files)
+        client.send(data)
+        file_name = client.recv(1024).decode("utf-8")
+        if os.path.exists(file_name):
+            client.send(bytes("True", "utf-8"))
+            with open(file_name,"rb") as file:
+                file_bytes = file.read()
+                client.send(file_bytes)
+        else:
+            client.send(bytes("False", "utf-8"))
+    else:
+        list_of_files = os.listdir(r"C:\Users\mitth\PycharmProjects\sockets")
+        data = pickle.dumps(list_of_files)
+        client.send(data)
+        file_head = client.recv(2**32).decode("utf-8")
+        file_data = client.recv(2**32)
+        with open(file_head, "wb") as f:
+            f.write(bytes(file_data))
+            f.close()
 
 
-server()
+while True:
+    connected_socket = s.accept()[0]
+    threading.Thread(target=server, args=(connected_socket, )).start()
